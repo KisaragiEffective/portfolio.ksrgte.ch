3ed77dcad605b547bf9864b7c87f0fda539ee383
Kisaragi <kisaragi.effective@gmail.com>
2022-01-27 23:40:43 +0900

    update: make https optional

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use actix_web::web::{JsonConfig};
 use actix_files::NamedFile;
 use rustls::{Certificate, PrivateKey, ServerConfig};
 use rustls_pemfile::{certs, pkcs8_private_keys};
+use anyhow::{anyhow, bail, Context, Result as AnyHowResult};
 
 async fn favicon() -> StdIoResult<NamedFile> {
     NamedFile::open("static/favicon.ico")
@@ -17,26 +18,28 @@ async fn index(req: HttpRequest) -> HttpResponse {
         .body("<!DOCTYPE html><html><body><p>Welcome!</p></body></html>")
 }
 
+fn load_certification_files() -> AnyHowResult<ServerConfig> {
+    println!("loading cert.pem");
+    let cert_file = &mut BufReader::new(File::open("cert.pem").context("cert.pem was not found")?);
+    let cert_chain = certs(cert_file).unwrap().iter().map(|a| Certificate(a.clone())).collect();
+    println!("loading key.pem");
+    let key_file = &mut BufReader::new(File::open("key.pem").context("key.pem was not found")?);
+    let mut keys = pkcs8_private_keys(key_file).unwrap().iter().map(|x| PrivateKey(x.clone())).collect::<Vec<_>>();
+    if keys.is_empty() {
+        bail!("Could not locate PKCS 8 private keys");
+    }
+
+    ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(cert_chain, keys.remove(0)).context("failed in with_single_cert ?!?!")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("starting");
     // load SSL keys
-    let mut config = {
-        println!("loading cert.pem");
-        let cert_file = &mut BufReader::new(File::open("cert.pem").unwrap());
-        let cert_chain = certs(cert_file).unwrap().iter().map(|a| Certificate(a.clone())).collect();
-        println!("loading key.pem");
-        let key_file = &mut BufReader::new(File::open("key.pem").unwrap());
-        let mut keys = pkcs8_private_keys(key_file).unwrap().iter().map(|x| PrivateKey(x.clone())).collect::<Vec<_>>();
-        if keys.is_empty() {
-            eprintln!("Could not locate PKCS 8 private keys.");
-            std::process::exit(1);
-        }
-        ServerConfig::builder()
-            .with_safe_defaults()
-            .with_no_client_auth()
-            .with_single_cert(cert_chain, keys.remove(0)).unwrap()
-    };
+    let mut config = load_certification_files();
 
     println!("Reading config...");
     println!("building HttpServer");
@@ -49,9 +52,19 @@ async fn main() -> std::io::Result<()> {
                     .finish()
             })))
     });
-    println!("binding ports");
+    println!("https");
+    // it is not required to enable https
+    match config {
+        Ok(cert_config) => {
+            http_server = http_server.bind_rustls("127.0.0.1:443", cert_config)?;
+        }
+        Err(error) => {
+            eprintln!("{:?}", error)
+        }
+    }
+
+    println!("http");
     http_server
-        .bind_rustls("127.0.0.1:443", config)?
         .bind("127.0.0.1:80")?
         .run()
         .await;
